50a952f2d069d83dfbf388087fe0da91315dbd06
Chaitanya <chaitanyasatarkar123@gmail.com>
2024-05-17 13:31:24 +0530

    testcase falling

--- src/App.js
+++ src/App.js
@@ -3,58 +3,50 @@ import './App.css';
 import { useEffect, useRef, useState } from 'react';
 
 function App() {
-
-  const [time, settime] = useState(0);
+  const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-  const ref =  useRef(null)
+  const ref = useRef(null);
 
-  useEffect(()=>{
-
+  useEffect(() => {
     if (isRunning) {
-     ref.current = setInterval(()=>{
-        settime((prev) => prev+10)
-      },10)
-    }
-
-    else if (!isRunning && ref.current){
+      ref.current = setInterval(() => {
+        setTime((prev) => prev + 10);
+      }, 10);
+    } else if (!isRunning && ref.current) {
       clearInterval(ref.current);
-      ref.current =  null
+      ref.current = null;
     }
 
     return () => clearInterval(ref.current);
-  },[isRunning])
+  }, [isRunning]);
 
-  const handlestart = () =>{
-    setIsRunning(true)
-  }
+  const handleStart = () => {
+    setIsRunning(true);
+  };
 
-  const handlereset= () => {
-    setIsRunning(false)
-    settime(0)
-  }
+  const handleReset = () => {
+    setIsRunning(false);
+    setTime(0);
+  };
 
-
-  const formatdata = (time) =>{
-    const data = (time/1000).toFixed(2)
-    return data
-  }
-
+  const formatData = (time) => {
+    const seconds = (time / 1000).toFixed(2);
+    return seconds;
+  };
 
   return (
     <div className="App">
       <div>
-        <h1>
-          STOPWATCH
-        </h1>
+        <h1>STOPWATCH</h1>
       </div>
 
       <div>
-        <span>Time :  </span>
-        <span>{formatdata(time)}</span>
+        <span>Time: </span>
+        <span>{formatData(time)}</span>
       </div>
       <span>
-        <button onClick={handlestart}>start</button>
-        <button onClick={handlereset}>reset</button>
+        <button onClick={handleStart}>start</button>
+        <button onClick={handleReset}>reset</button>
       </span>
     </div>
   );
